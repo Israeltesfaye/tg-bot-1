9d16005111cacf843a823d605377e997a9568579
Israel tesfaye <israeltesfaye250@gmail.com>
2023-05-16 09:46:32 -0900

    main keywords

--- index.js
+++ index.js
@@ -10,10 +10,32 @@ const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, (msg) => {
 
-bot.sendMessage(msg.chat.id, "Welcome", {
+bot.sendMessage(msg.chat.id,"welcome",{
 "reply_markup": {
-    "keyboard": [["join", "invite friends"]]
+    "keyboard": [["join group"],["invite friends"],["courses"],["status"]]
     }
 });
 
 });
+
+
+bot.on('message', (msg) => {
+
+if (msg.text.toString().toLowerCase().indexOf("join group") === 0) {
+bot.sendMessage(msg.chat.id,"join group");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("invite friends") === 0) {
+bot.sendMessage(msg.chat.id,"join group");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("courses") === 0) {
+bot.sendMessage(msg.chat.id,"join group");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("status") === 0) {
+bot.sendMessage(msg.chat.id,"status");
+}
+
+});
+
