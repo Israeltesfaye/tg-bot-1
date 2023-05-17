6892393ad2ef8a26b09739454648ec0e84aabe99
Israel tesfaye <israeltesfaye250@gmail.com>
2023-05-17 08:47:26 -0900

    links

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token=process.env.BOT_TOKEN;
 
 
+app.use(express.static("./public"))
+
+
 
 app.get('/join',(req,res)=>{
     res.send("join")
@@ -22,6 +25,7 @@ bot.sendMessage(msg.chat.id,"welcome",{
     }
 });
 
+
 });
 
 
@@ -37,11 +41,33 @@ bot.sendMessage(msg.chat.id,"join group");
 }
 
 if (msg.text.toString().toLowerCase().indexOf("courses") === 0) {
-bot.sendMessage(msg.chat.id,"join group");
+
+    bot.sendMessage(msg.chat.id,"courses available",{
+        "reply_markup":{
+   "keyboard":[["Python"],["html/css"],["Javascript"],["Python for Machine Learning"],["Javascript for frontend"],["git"],["Linux/Unix"]]
+        }
+    })
 }
 
 if (msg.text.toString().toLowerCase().indexOf("status") === 0) {
 bot.sendMessage(msg.chat.id,"status");
+}
+
+
+if (msg.text.toString().toLowerCase().indexOf("git") === 0 || msg.text.toString().toLowerCase().indexOf("Linux/Unix") === 0 ) {
+bot.sendMessage(msg.chat.id,"https://t.me/+eb360SlsDkFjYmY0");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("Javascript") === 0 || msg.text.toString().toLowerCase().indexOf("Javascript for frontend") === 0 ) {
+bot.sendMessage(msg.chat.id,"https://t.me/+9m5HeT6FzhRhMGVk");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("html/css") === 0) {
+bot.sendMessage(msg.chat.id,"https://t.me/+QX9-Y_DZzCExNmVk");
+}
+
+if (msg.text.toString().toLowerCase().indexOf("Python") === 0 || msg.text.toString().toLowerCase().indexOf("Python for Machine Learning") === 0 ) {
+bot.sendMessage(msg.chat.id,"https://t.me/+DLuvA3c5p0YxMDI0");
 }
 
 });
---
+++ public/script.js
@@ -0,0 +1,19 @@
+container=document.querySelector('.container')
+window.question=document.querySelector('.question')
+window.answer=document.querySelectorAll('.answer').forEach((a)=>{
+  a.onclick=()=>{
+  alert("4444444")
+}
+})
+answers=document.querySelector('.answers')
+
+db=[
+{
+ question:"how old are you?",
+ answers=[""]
+}
+  ]
+
+window.addEventListener('load',function(){
+  console.log("hdhdhdh")
+})
